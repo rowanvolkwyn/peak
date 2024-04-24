82a7a2fad9b3554461d82b9a7c33eda7ce5bc6e0
Rowan <volkwyn.rowan@gmail.com>
2024-04-24 14:29:34 +1000

    added basic structure styling for entrey form

--- src/components/EntryForm.jsx
+++ src/components/EntryForm.jsx
@@ -1,23 +1,25 @@
+import './EntryForm.css';
+
 const EntryForm = () => {
     return (
         <div id='card'>
             <h2 id='title'>New Entry</h2>
             <div id='exercise'>
-                <h6>Exercise</h6>
-                <input></input>
+                <h6>Exercise:</h6>
+                <input id='exercise-input'></input>
                 <h6>Set 1</h6>
                 <div id='sets'>
                     <div class='sets-info' id='weight'>
-                        <p>Weight (kg)</p>
-                        <input></input>
+                        <p>Weight (KG)</p>
+                        <input class='input-info'></input>
                     </div>
                     <div class='sets-info' id='reps'>
                         <p>Reps</p>
-                        <input></input>
+                        <input class='input-info'></input>
                     </div> 
                     <div class='sets-info' id='rpe'>
                         <p>RPE</p>
-                        <input></input>
+                        <input class='input-info'></input>
                     </div>
                 </div>
             </div>
