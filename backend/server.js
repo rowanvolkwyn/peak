869b2aa11721a521c6344b4b0a16a1b21ea6f643
Rowan <volkwyn.rowan@gmail.com>
2024-05-15 16:02:16 +1000

    restructuring app

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+})
+
+// routes
+app.get('/', (req, res) => {
+    res.json({mssg: 'Welcome to the app'})
+})
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`)
+});
+
