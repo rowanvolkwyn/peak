2b8cadf05e5f79c1690bec020bae30089e98adf0
Rowan <volkwyn.rowan@gmail.com>
2024-05-23 22:47:03 +1000

    used dispatch to enable deleting excercises

--- frontend/src/components/WorkoutDetails.jsx
+++ frontend/src/components/WorkoutDetails.jsx
@@ -1,10 +1,26 @@
-const WorkoutDetails = ({ workout }) => {
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
+
+const WorkoutDetails = ({ workout }) => {   
+    const { dispatch } = useWorkoutsContext()
+
+    const handleClick = async () => {
+        const response = await fetch('/api/workouts/' + workout._id, {
+            method: 'DELETE'
+        })
+        const json = await response.json()
+
+        if (response.ok) {
+            dispatch({type: 'DELETE_WORKOUT', payload: json})
+        }
+    }
+
     return (
         <div className="workout-details">
             <h4>{workout.title}</h4>
             <p><strong>Load: </strong>{workout.load} kg</p>
             <p><strong>Reps: </strong>{workout.reps}</p>
             <p>{workout.createdAt}</p>
+            <span onClick={handleClick}>Delete</span>
         </div>
     )
 }
